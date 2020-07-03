163394b406710a637bfd129467647186f1a94905
venkattt123 <60930315+venkattt123@users.noreply.github.com>
2020-07-03 17:33:12 +0530

    Add files via upload

---
+++ list_for2.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+for var_name in $(seq 1 10  100); do
+  echo "Counting $var_name..."
+done
---
+++ nested_ifs.sh
@@ -0,0 +1,27 @@
+#!bin/bash
+
+#Program on Nested if statements
+
+echo "please enter the value for i:"
+
+read i
+
+if [ $i -ge 100 ]
+
+then
+	
+	echo "Its a highest number"
+	
+		if(( $i % 2 == 0))
+	
+		then
+	   
+		echo " it is also even number"
+	
+		else
+	   
+		echo "sorry it is not even number"
+	
+		fi
+
+fi
